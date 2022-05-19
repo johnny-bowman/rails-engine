25962cebab83d7bcf2831414372ba736aa21007b
Johnny Bowman <johntbowman.production@gmail.com>
2022-05-18 20:38:19 -0600

    update environment

---
+++ app/models/invoice_item.rb
@@ -0,0 +1,8 @@
+class InvoiceItem < ApplicationRecord
+  belongs_to :invoice
+  belongs_to :item
+
+  has_one :merchant, through: :item
+  has_many :transactions, through: :invoice
+  has_many :customers, through: :invoice
+end
