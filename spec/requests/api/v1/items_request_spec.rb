6a4bc1583b57f03690d4374393976f7935dc0a32
Johnny Bowman <johntbowman.production@gmail.com>
2022-05-19 13:23:48 -0600

    API: item create

--- spec/requests/api/v1/items_request_spec.rb
+++ spec/requests/api/v1/items_request_spec.rb
@@ -46,4 +46,25 @@ RSpec.describe "Items API" do
     expect(item[:attributes]).to have_key(:unit_price)
     expect(item[:attributes][:unit_price]).to be_a(Float)
   end
+
+  it "creates an item" do
+    merchant = create(:merchant)
+     item_params = ({
+       name: "Cam",
+       description: "Don't die, use this!",
+       unit_price: 204.40,
+       merchant_id: merchant.id
+     })
+    headers = {"CONTENT_TYPE" => "application/json"}
+
+    post "/api/v1/items", headers: headers, params: JSON.generate(item: item_params)
+
+    created_item = Item.last
+
+    expect(response.status).to eq(201)
+    expect(created_item.name).to eq(item_params[:name])
+    expect(created_item.description).to eq(item_params[:description])
+    expect(created_item.unit_price).to eq(item_params[:unit_price])
+    expect(created_item.merchant_id).to eq(item_params[:merchant_id])
+  end
 end
