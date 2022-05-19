82057b02d154b3d60aede560eb73bd5ee84ebff7
Johnny Bowman <johntbowman.production@gmail.com>
2022-05-19 12:47:59 -0600

    API: items index and show

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -3,5 +3,6 @@ FactoryBot.define do
     association :merchant
     name { Faker::Commerce.product_name}
     description { Faker::Commerce.material }
-    unit_price { Faker::Number.decimal(r_digits: 2) }  end
+    unit_price { Faker::Number.decimal(r_digits: 2) }
+  end
 end
---
+++ spec/requests/api/v1/items_request_spec.rb
@@ -0,0 +1,49 @@
+require "rails_helper"
+
+RSpec.describe "Items API" do
+  it "sends a list of items" do
+    create_list(:item, 3)
+
+    get "/api/v1/items"
+    expect(response).to be_successful
+
+    items = JSON.parse(response.body, symbolize_names: true)[:data]
+
+    expect(items.count).to eq(3)
+
+    items.each do |item|
+      expect(item).to have_key(:id)
+      expect(item[:id]).to be_an(String)
+
+      expect(item[:attributes]).to have_key(:name)
+      expect(item[:attributes][:name]).to be_a(String)
+
+      expect(item[:attributes]).to have_key(:description)
+      expect(item[:attributes][:description]).to be_a(String)
+
+      expect(item[:attributes]).to have_key(:unit_price)
+      expect(item[:attributes][:unit_price]).to be_a(Float)
+    end
+  end
+
+  it "sends one item" do
+    id = create(:item).id
+
+    get "/api/v1/items/#{id}"
+    expect(response).to be_successful
+
+    item = JSON.parse(response.body, symbolize_names: true)[:data]
+
+    expect(item).to have_key(:id)
+    expect(item[:id]).to be_an(String)
+
+    expect(item[:attributes]).to have_key(:name)
+    expect(item[:attributes][:name]).to be_a(String)
+
+    expect(item[:attributes]).to have_key(:description)
+    expect(item[:attributes][:description]).to be_a(String)
+
+    expect(item[:attributes]).to have_key(:unit_price)
+    expect(item[:attributes][:unit_price]).to be_a(Float)
+  end
+end
